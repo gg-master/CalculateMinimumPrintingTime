fa4767dad3f64aef84b2baa144fe49e56c4358ab
gg-master <korshun.tolik@yandex.ru>
2023-05-09 14:25:08 +0300

    Added header documentation

--- CalculateMinimumPrintingTime/CalculateMinimumPrintingTime.h
+++ CalculateMinimumPrintingTime/CalculateMinimumPrintingTime.h
@@ -16,16 +16,44 @@ struct CalcMinPrintingTimeParams
 };
 
 
+class InvalidValueException;
+
+class InvalidInputFileException;
+
+
+/*! –ассчитывает минимальное врем€ на печать копий документов на двух принтерах
+	с учетом веро€тностей выхода принтеров из стро€.
+* \param[in] params - параметры задачи, объединенные в структуру.
+* \return минимальное врем€ на печать заданного количества копий документов.
+*/
 int calcMinPrintingTime(CalcMinPrintingTimeParams params);
 
 
+/*! ѕодсчет количества подстрок в строке, разделенных пробелами или табул€цией.
+* \param[in] input_line - строка, в которой производитс€ подсчет подстрок.
+* \return  оличество подстрок в строке, разделенных пробелами или табул€цией.
+*/
 int countValuesInLine(const std::string& input_line);
 
 
+/*! ѕрочитывает входные данные из файла и сохран€ет их в структуру CalcMinPrintingTimeParams.
+* \param[in] input_file_path - строка с путем к файлу с входными данными.
+* \param[out] params - параметры решаемой задачи, объединенные в одну структуру.
+*/
 void readInputFile(const std::string& input_file_path, CalcMinPrintingTimeParams* params);
 
 
+/*! ѕровер€ет принадлежность значени€ указанному диапазону.
+* \param[in] value - провер€емое значение.
+* \param[in] left_border - лева€ граница диапазона.
+* \param[in] right_border - права€ граница диапазоне.
+* \return «начение true, если значение принадлежит диапазону, иначе - false.
+*/
 bool isInRange(double value, double left_border, double right_border);
 
 
+/*! ѕроводит валидацию входных параметров согласно ограничен€ми.
+	ѕри провале проверки, выбрасывает ошибку валидации.
+* \param[in] params - валидируемые параметры задачи, объединенные в структуру.
+*/
 void validateInputData(const CalcMinPrintingTimeParams params);
